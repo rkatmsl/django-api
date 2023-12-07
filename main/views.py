33fcf6f5f45e2662c70f219c0d29cda6c36e88c7
rkatmsl <153056657+rkatmsl@users.noreply.github.com>
2023-12-07 17:40:49 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -10,7 +10,9 @@ def index(request):
 def store(request, device_id):
     try:
         visitor = Users.objects.get(device_id=device_id)
+        visitor.visits += 1
+        visitor.save()
         return JsonResponse({'exists': True})
     except Users.DoesNotExist:
-        new_visitor = Users.objects.create(device_id=device_id)
+        new_visitor = Users.objects.create(device_id=device_id, visits=0)
         return JsonResponse({'exists': False})  
