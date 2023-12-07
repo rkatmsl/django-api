a84c4f49ecf20bdd6fd60acb431e00b56306be84
rkatmsl <153056657+rkatmsl@users.noreply.github.com>
2023-12-07 17:41:11 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Users(models.Model):
     device_id = models.CharField(max_length=200)
+    visits = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = "Users"
